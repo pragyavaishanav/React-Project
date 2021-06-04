eee0be864fcdcb6cf509b63d31232a215625c9c6
Rahul Vaishnav <rvaishnav@neuratwin.com>
2021-06-03 22:22:47 -0700

    Download component added to main page to view

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import Header from '../sections/header/header.component';
-import AnimScroll from '../sections/animator/animator.component'
+import React from "react";
+import Header from "../sections/header/header.component";
+import AnimScroll from "../sections/animator/animator.component";
+import Download from "../sections/download/download.component";
 
 const Homepage = () => (
-    <div>
-        <Header></Header>
-        <AnimScroll></AnimScroll>
-    </div>
-
+  <div>
+    <Header></Header>
+    <AnimScroll></AnimScroll>
+    <Download></Download>
+  </div>
 );
 
 export default Homepage;
