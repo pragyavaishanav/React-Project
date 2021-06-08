9f9e37203459ffc489adca7ec0e4cb41c6b97c8b
garvi <garvit0720@gmail.com>
2021-06-08 09:05:27 +0530

    added card component to the header page

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../sections/header/header.component';
 import AnimScroll from '../sections/animator/animator.component';
 import Footer from '../sections/footer/footer.component';
+import Card from '../sections/card/card.component';
 
 const Homepage = () => (
     <div>
         <Header></Header>
         <AnimScroll></AnimScroll>
+        <Card></Card>
         <Footer></Footer>
     </div>
 
