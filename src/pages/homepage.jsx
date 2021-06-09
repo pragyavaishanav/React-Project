c9bcaaf93d4a3a5288ca2acd7100b0361307b352
garvi <garvit0720@gmail.com>
2021-06-09 13:22:56 +0530

    added card2

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,12 +3,14 @@ import Header from '../sections/header/header.component';
 import AnimScroll from '../sections/animator/animator.component';
 import Footer from '../sections/footer/footer.component';
 import Card from '../sections/card/card.component';
+import Card2 from '../sections/card2/card2.component';
 
 const Homepage = () => (
     <div>
         <Header></Header>
         <AnimScroll></AnimScroll>
         <Card></Card>
+        <Card2></Card2>
         <Footer></Footer>
     </div>
 
