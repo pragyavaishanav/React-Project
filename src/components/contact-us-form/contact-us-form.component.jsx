9c09861fd5a2721759d4c152a798d8ecb802c4b3
Rahul Vaishnav <rvaishnav@neuratwin.com>
2021-06-24 15:20:56 +0530

    contact form added

--- src/components/contact-us-form/contact-us-form.component.jsx
+++ src/components/contact-us-form/contact-us-form.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import emailjs from "emailjs-com";
-import './contact-us-form.styles.scss'
+import "./contact-us-form.styles.scss";
 
 const initialState = {
   firstName: "",
@@ -11,20 +11,18 @@ const initialState = {
   lastNameError: "",
   emailError: "",
   messageError: "",
-  classname: "hide-error"
+  classname: "hide-error",
 };
 
 export default class ValiationForm extends React.Component {
   state = initialState;
 
-  handleChange = e => {
+  handleChange = (e) => {
     const isCheckbox = e.target.type === "checkbox";
     this.setState({
-      [e.target.name]: isCheckbox
-        ? e.target.checked
-        : e.target.value
+      [e.target.name]: isCheckbox ? e.target.checked : e.target.value,
     });
-    this.setState({classname: "hide-error"})
+    this.setState({ classname: "hide-error" });
   };
 
   validate = () => {
@@ -48,30 +46,43 @@ export default class ValiationForm extends React.Component {
     if (!this.state.email.includes("@")) {
       emailError = "Invalid email address";
     }
-    
+
     if (emailError || firstNameError || lastNameError || messageError) {
-      this.setState({ emailError, firstNameError , lastNameError, messageError});
-      this.setState({classname: "errormsg"})
+      this.setState({
+        emailError,
+        firstNameError,
+        lastNameError,
+        messageError,
+      });
+      this.setState({ classname: "errormsg" });
       return false;
     }
 
     return true;
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
     const isValid = this.validate();
     if (isValid) {
-        emailjs.sendForm('service_6se4ao7', 'template_gnhvh88', e.target, 'user_UvAGTO1QXyVLDBxrjlJmo')
-        .then((result) => {
+      emailjs
+        .sendForm(
+          "service_i4hdzzx",
+          "template_mk8pept",
+          e.target,
+          "user_LmTZzaEnj07BsKfdCeLNa"
+        )
+        .then(
+          (result) => {
             console.log(result.text);
-        }, (error) => {
+          },
+          (error) => {
             console.log(error.text);
-        });
-        alert("form submitted");
-        this.setState(initialState);
-    }
-    else{
+          }
+        );
+      alert("form submitted");
+      this.setState(initialState);
+    } else {
       console.log(this.state);
       alert("Please enter correct details.");
     }
@@ -79,67 +90,71 @@ export default class ValiationForm extends React.Component {
 
   render() {
     return (
-        <div className="contact-us-form-container">
-            <div className="contact-us-form-title">How can we help you?</div>
+      <div className="contact-us-form-container">
+        <div className="contact-us-form-title">How can we help you?</div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <label className="forms-style" >First Name*</label>
-                        <input
-                            name="firstName"
-                            placeholder="First Name"
-                            className="input"
-                            value={this.state.firstName}
-                            onChange={this.handleChange}
-                        />
-                        <div className={this.state.classname} >
-                            <label className="error-label" >{this.state.firstNameError}</label>
-                        </div>
-                    </div>
-                    <div>
-                        <label className="forms-style" >Last Name*</label>
-                        <input
-                            name="lastName"
-                            placeholder="Last Name"
-                            className="input"
-                            value={this.state.lastName}
-                            onChange={this.handleChange}
-                        />
-                        <div className={this.state.classname} >
-                            <label className="error-label" >{this.state.lastNameError}</label>
-                        </div>
-                    </div>
-                    <div>
-                        <label className="forms-style" >Email Address*</label>
-                        <input
-                            name="email"
-                            placeholder="e.g user@gmail.com"
-                            className="input"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                        <div className={this.state.classname} >
-                            <label className="error-label" >{this.state.emailError}</label>
-                        </div>
-                    </div>
-                    <div>
-                        <label className="forms-style" >Message*</label>
-                        <textarea
-                            rows="1"
-                            name="message"
-                            placeholder="Please type your message"
-                            className="input"
-                            value={this.state.message}
-                            onChange={this.handleChange}
-                        />
-                        <div className={this.state.classname} >
-                            <label className="error-label" >{this.state.messageError}</label>
-                        </div>
-                    </div>
-                    <input className="input-submit-btn" type="submit" />
-                </form>
+              <label className="forms-style">First Name*</label>
+              <input
+                name="firstName"
+                placeholder="First Name"
+                className="input"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+              <div className={this.state.classname}>
+                <label className="error-label">
+                  {this.state.firstNameError}
+                </label>
+              </div>
             </div>
+            <div>
+              <label className="forms-style">Last Name*</label>
+              <input
+                name="lastName"
+                placeholder="Last Name"
+                className="input"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+              <div className={this.state.classname}>
+                <label className="error-label">
+                  {this.state.lastNameError}
+                </label>
+              </div>
+            </div>
+            <div>
+              <label className="forms-style">Email Address*</label>
+              <input
+                name="email"
+                placeholder="e.g user@gmail.com"
+                className="input"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <div className={this.state.classname}>
+                <label className="error-label">{this.state.emailError}</label>
+              </div>
+            </div>
+            <div>
+              <label className="forms-style">Message*</label>
+              <textarea
+                rows="1"
+                name="message"
+                placeholder="Please type your message"
+                className="input"
+                value={this.state.message}
+                onChange={this.handleChange}
+              />
+              <div className={this.state.classname}>
+                <label className="error-label">{this.state.messageError}</label>
+              </div>
+            </div>
+            <input className="input-submit-btn" type="submit" />
+          </form>
         </div>
+      </div>
     );
   }
 }
